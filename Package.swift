4259764f4934107d3a62bd58a508c488d3b3767a
marcprux <mwp1@cornell.edu>
2023-10-06 20:07:19 -0400

    Export dependencies in build.gradle.kts as api

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "SkipLib", type: .dynamic, targets: ["SkipLib"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.6.88"),
-        .package(url: "https://source.skip.tools/skip-unit.git", from: "0.3.8"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.6.96"),
+        .package(url: "https://source.skip.tools/skip-unit.git", from: "0.3.10"),
     ],
     targets: [
         .target(name: "SkipLib", dependencies: [.product(name: "SkipUnit", package: "skip-unit")], plugins: [.plugin(name: "skipstone", package: "skip")]),
