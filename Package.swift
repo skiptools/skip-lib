e24ca526c79521b3c1635c8ca8e6c4b35bd97e00
marcprux <mwp1@cornell.edu>
2024-02-05 16:12:38 -0500

    Use gradle version catalogs for dependencies

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "SkipLib", targets: ["SkipLib"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.7.52"),
-        .package(url: "https://source.skip.tools/skip-unit.git", from: "0.4.8"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.8.0"),
+        .package(url: "https://source.skip.tools/skip-unit.git", from: "0.5.0"),
     ],
     targets: [
         .target(name: "SkipLib", dependencies: [.product(name: "SkipUnit", package: "skip-unit")], plugins: [.plugin(name: "skipstone", package: "skip")]),
