3773e4abc686d00a927e122e786005b2c6cecaf4
Abe White <abewhite@mac.com>
2024-11-18 11:11:15 -0600

    Bridge support cleanup

--- Sources/SkipLib/BridgeSupport.swift
+++
@@ -1,9 +0,0 @@
-// Copyright 2024 Skip
-//
-// This is free software: you can redistribute and/or modify it
-// under the terms of the GNU Lesser General Public License 3.0
-// as published by the Free Software Foundation https://fsf.org
-
-/// Marker protocol for types that are custom bridged from the Swift side.
-public protocol SwiftCustomBridged {
-}
